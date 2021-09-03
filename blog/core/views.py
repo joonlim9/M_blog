81e6c551dc9280bcb7f13f8e041d698ab5a8c5ae
joonlim9 <joonlim@umich.edu>
2021-09-02 21:13:52 -0400

    Blog_post + views

--- blog/core/views.py
+++ blog/core/views.py
@@ -1,7 +1,14 @@
 from flask import render_template,request,Blueprint
+from blog.models import BlogPost
 
 core = Blueprint('core',__name__)
 
 @core.route('/')
 def info():
     return render_template('info.html')
+
+@core.route('/blog_posts')
+def info():
+    page = request.args.get('page', 1, type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html')
